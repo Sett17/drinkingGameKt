a0a279204d40a2ad22d6cd7b5e8009e1149e5b3f
Sett17 <raik.rohde@gmail.com>
2021-05-15 00:27:57 +0200

    moved ssot

--- src/main/resources/pwabuilder-sw.js
+++ src/main/resources/pwabuilder-sw.js
@@ -1,4 +1,7 @@
 // This is the "Offline copy of assets" service worker
+
+const CACHE = 'v1.0.1'
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 self.addEventListener("message", (event) => {
